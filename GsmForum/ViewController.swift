bf949ad5e1b76b227b671f893e511bca85aa7803
uuuunseo <jdbstjoo@gmail.com>
2022-10-26 20:00:04 +0900

    로그인 페이지 기본 틀

--- GsmForum/ViewController.swift
+++ GsmForum/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate{
 
+
+    @IBOutlet weak var textfieldEmailid: UITextField!
+    
+    
+    @IBOutlet weak var textfieldpassword: UITextField!
+    
+    @IBAction func showpass(_ sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textfieldEmailid.delegate = self
+        textfieldpassword.delegate = self
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.textfieldEmailid.resignFirstResponder()
+        self.textfieldpassword.resignFirstResponder()
+        return true
     }
 
-
+    @IBAction func buttonsignin(_ sender: Any) {
+    }
+    
+    func isValidEmail(testStr:String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format: "SELF MATCHES", emailRegEx)
+        return emailTest.evaluate(with: testStr)
+    }
 }
 
